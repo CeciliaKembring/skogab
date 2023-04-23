a61ca7424313564b7c9bf15353c3524dec997471
Cecilia Kembring <ceke2200@student.miun.se>
2023-04-23 09:08:21 +0200

    navbar mobile

--- main.js
+++ main.js
@@ -4,9 +4,11 @@ let navEl = document.querySelector(".navbar");
 menuBtnEl.addEventListener("click", menubar);
 
 function menubar(){
-    if(navEl.style.display ==="none"){
-        navEl.style.display = "block";
-    } else {
+    if(navEl.style.display ==="block"){
         navEl.style.display = "none";
+    } else {
+        navEl.style.display = "block";
     }
 }
+
+
